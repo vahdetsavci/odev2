3b10b6774c53818f2d05b4c52d5d9c4dbfbf532f
Vahdet Savci <savcivahdet@gmail.com>
2023-11-09 12:42:43 +0300

    third app is done

--- Koleksiyonlar-Soru-3/Program.cs
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace Koleksiyonlar_Soru_3;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.Write("Bir cümle giriniz: ");
+        char[] sesliHarfler = Console.ReadLine().ToLower().SesliHarfBul();
+        Array.Sort(sesliHarfler);
+
+        Console.Write("Sesli Harfler: ");
+        foreach (char harf in sesliHarfler)
+            Console.Write(harf + " ");
+    }
+}
+
+static class MyClass
+{
+    internal static char[] SesliHarfBul(this string param)
+    {
+        char[] sesliHarfler = {'a','e','ı','i','o','ö','u','ü'};
+        char[] bulunanlar = new char[param.Length];
+        int count = 0;
+
+        for (int i = 0; i < sesliHarfler.Length; i++)
+        {
+            for (int j = 0; j < param.Length; j++)
+            {
+                if (sesliHarfler[i] == param[j])
+                {
+                    bulunanlar[count] = param[j];
+                    count++;
+                }
+            }
+        }
+
+        Array.Resize<char>(ref bulunanlar, count);
+        return bulunanlar;
     }
 }
